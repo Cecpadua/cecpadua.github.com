6b76b515a361684ea86197e6ba58a42f1a0aa8c2
yihao <yihao.zhuo.2001@gmail.com>
2017-12-12 09:50:55 +0100

    更新

--- js/init.js
+++ js/init.js
@@ -3,6 +3,8 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+	$('.collapsible').collapsible();
+    $('.scrollspy').scrollSpy();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
